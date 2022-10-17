9d14ae9d8a08254fe22c0434517a01f2d3a13daa
Giri Gimnastiar <girigimnastiarwork@gmail.com>
2022-10-17 14:50:25 +0700

    menambahkan function button delete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,17 @@
 const books = [];
 const RENDER_EVENT = "render-todo";
 
+const SAVED_EVENT = "saved-todo";
+const STORAGE_KEY = "TODO_APPS";
+
+function isStorageExist() /* boolean */ {
+  if (typeof Storage === undefined) {
+    alert("Browser kamu tidak mendukung local storage");
+    return false;
+  }
+  return true;
+}
+
 function Read() {
   return document.getElementById("checkbook").checked;
 }
@@ -12,14 +23,39 @@ function undofile(idbook) {
 
   bookTarget.isRead = false;
   document.dispatchEvent(new Event(RENDER_EVENT));
+  saveBook();
 }
 
 function deleteBook(idbook) {
-  const bookTarget = getBook(idbook);
+  const bookTarget = findBookIndex(idbook);
 
   if (bookTarget === -1) return;
 
   books.splice(bookTarget, 1);
+  document.dispatchEvent(new Event(RENDER_EVENT));
+  saveBook();
+}
+
+function findBookIndex(idbook) {
+  for (const index in books) {
+    if (books[index].id === idbook) {
+      return index;
+    }
+  }
+
+  return -1;
+}
+
+function loadDataFromStorage() {
+  const serializedData = localStorage.getItem(STORAGE_KEY);
+  let data = JSON.parse(serializedData);
+
+  if (data !== null) {
+    for (const book of data) {
+      books.push(book);
+    }
+  }
+
   document.dispatchEvent(new Event(RENDER_EVENT));
 }
 
@@ -29,6 +65,13 @@ document.addEventListener("DOMContentLoaded", function () {
     Event.preventDefault();
     addBook();
   });
+  if (isStorageExist()) {
+    loadDataFromStorage();
+  }
+});
+
+document.addEventListener(SAVED_EVENT, function () {
+  console.log(localStorage.getItem(STORAGE_KEY));
 });
 
 function addBook() {
@@ -39,6 +82,7 @@ function addBook() {
   const bookObject = generateBookObject(generatedID, bookName, bookCreated, bookYear, Read());
   books.push(bookObject);
   document.dispatchEvent(new Event(RENDER_EVENT));
+  saveBook();
 }
 
 function generateBookObject(id, bookName, bookCreated, bookYear, isRead) {
@@ -105,6 +149,14 @@ function getBook(idbook) {
     }
   }
   return null;
+}
+
+function saveBook() {
+  if (isStorageExist()) {
+    const parsed = JSON.stringify(books);
+    localStorage.setItem(STORAGE_KEY, parsed);
+    document.dispatchEvent(new Event(SAVED_EVENT));
+  }
 }
 
 document.addEventListener(RENDER_EVENT, function () {
